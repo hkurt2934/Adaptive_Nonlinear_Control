6a36300fae9dd2983917f3f277a79f95c7603115
hkurt2934 <hkurt2934@gmail.com>
2024-07-23 10:39:28 +0300

    Update 23-08-2024

--- Article_Active_Suspension_Control/Fuzzy_Control_Dataset.m
+++ Article_Active_Suspension_Control/Fuzzy_Control_Dataset.m
@@ -12,7 +12,7 @@ accel_1_1 = mf_gauss_raod_1.out.Accleration;
 accel_1_2 = mf_tri_gauss_raod_1.out.Accleration;
 accel_1_3 = mf_tri_equ_raod_1.out.Accleration;
 accel_1_4 = mf_trape_raod_1.out.Accleration;
-
+road_2 = mf_gauss_raod_2.out.Road_Disturbance;
 figure(1);
 xlabel('Time (seconds)', 'FontWeight','bold');
 ylabel('Car Body Acceleration (m/s^2)', 'FontWeight','bold');
@@ -92,3 +92,32 @@ plot(accel_2_1, 'LineWidth' , 2, 'Color', 'Blue');
 legend("Triangular Gaussion", "Trapezoidal", "Triangular Equal","Gaussian", 'FontWeight','bold');
 xlim([0,10]);
 ylim([-1,1]);
+%%
+figure(6);
+road_2_1 = road_2;
+road_2_1.Data = 0;
+xlabel('Time (seconds)', 'FontWeight','bold');
+ylabel('Road Disturbance (m)', 'FontWeight','bold');
+grid( 'on');
+hold( 'on');
+plot(road_1, 'LineWidth' , 3, 'Color', 'Red');
+xlim([0,10]);
+ylim([-0.05,0.15]);
+
+%%
+figure(7);
+road_2_1 = road_2;
+road_2_1.Data = 0;
+xlabel('Time (seconds)', 'FontWeight','bold');
+ylabel('Road Disturbance (m)', 'FontWeight','bold');
+grid('on');
+hold('on');
+plot(road_2_1, 'LineWidth' , 1.5, 'Color', 'Black');
+plot(road_2, 'LineWidth' , 3, 'Color', 'Red');
+xlim([0,10]);
+ylim([-0.05,0.05]);
+
+
+
+
+
